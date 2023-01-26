97e30196df1081d574fc48e8260f3749a8b20536
Elihu Rodriguez <elihu.rodriguez@outlook.com>
2023-01-26 11:41:37 -0800

    Added Contacts its own context file and hook file. Updated app index to include contact provider.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import { JobsContextProvider } from './context/JobsContext.js';
 import { StatusesContextProvider } from './context/StatusesContext.js'
+import { ContactsContextProvider } from './context/ContactsContext.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <StatusesContextProvider>
-      <JobsContextProvider>
-        <App />
-      </JobsContextProvider>
+      <ContactsContextProvider>
+        <JobsContextProvider>
+          <App />
+        </JobsContextProvider>
+      </ContactsContextProvider>
     </StatusesContextProvider>
   </React.StrictMode>
 );
