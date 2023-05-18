2aa11acdacbef406a529e531dbbc009d26a3c36d
Elihu Rodriguez <elihu.rodriguez@outlook.com>
2023-05-18 13:19:49 -0700

    Revert "Updated variables"
    This reverts commit 33bbdc63968dfed75131d6959fe72df51374c737.

--- backend/services/email.js
+++ backend/services/email.js
@@ -30,7 +30,7 @@ export const sendResetPasswordLink = async ({ firstName, email, token }) => {
             from: process.env.EMAIL_ACCOUNT_SENDER_T0_REGISTER_NEW_USER,
             to: email,
             subject: `Moving Forward. You requested to reset your password.`,
-            html: `Hello ${firstName},<br/><br/>The link below will reset your password, this link expires in one day after this message has been sent.<br />If you did not request this, you may ignore this message.<br/>Your account has not been changed.<br/>Please note, once you click the link, you'll have to set up your new password before you can login to Moving Forward. Contact us if this link expires.<br/><br/><a href="http://localhost:6001/verify/${token}/${firstName}/1">Reset my password with Moving Forward.</a><br/><br/> Best regards,<br/>Moving Forward, LLC.`
+            html: `Hello ${firstName},<br/><br/>The link below will reset your password, this link expires in one day after this message has been sent.<br />If you did not request this, you may ignore this message.<br/>Your account has not been changed.<br/>Please note, once you click the link, you'll have to set up your new password before you can login to Moving Forward. Contact us if this link expires.<br/><br/><a href="http://localhost:3000/verify/${token}/${firstName}/1">Reset my password with Moving Forward.</a><br/><br/> Best regards,<br/>Moving Forward, LLC.`
         });
     }
     catch (err) {
