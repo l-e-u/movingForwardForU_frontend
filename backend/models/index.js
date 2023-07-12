f484b0ae998d34a35abfd337ecd73d50d929604b
Elihu Rodriguez <120612676+l-e-u@users.noreply.github.com>
2023-07-12 08:15:11 -0700

    Delete backend directory
    Backend will be in its own repository.

--- backend/models/index.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-mongoose.set('strictQuery', false);
-
-const connectToDatabase = () => {
-    return mongoose.connect(process.env.MONGO_URI);
-};
-
-export { connectToDatabase };
